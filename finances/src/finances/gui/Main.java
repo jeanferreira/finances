9045778e645e2cab957f541f632b79d000664c1b
jeanferreira <jeansiferreira@gmail.com>
2016-04-13 19:56:52 -0300

    Criação tela home

--- finances/src/finances/gui/Main.java
+++ finances/src/finances/gui/Main.java
@@ -1,9 +1,40 @@
 package finances.gui;
 
-public class Main {
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+import finances.view.JFHome;
+
+
+
+
+public class Main extends SingleFrameApplication {
+	
+	private static JFHome jhome;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(Main.class, args);
+
+	}
+	
+	public static Main getApplication() {
+		return Application.getInstance(Main.class);
+	}
+
+	@Override
+	protected void configureWindow(java.awt.Window root) {
+	}
+
+
+	@Override
+	protected void startup() {
+		if (jhome == null) {
+			jhome = new JFHome();
+			jhome.setVisible(true);
+
+		} else {
+			jhome.setVisible(true);
+		}
 
 	}
 
